168366226fbf67cef02a3f446c9eb28b1b67e163
coder-abhk <abhkrwt07@gmail.com>
2021-02-13 12:10:20 +0530

    birthday reminder app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [birthdays, setBirthdays] = useState([
+    { id: 1, name: "Sonny", born: "09/03/2001" },
+    { id: 2, name: "Baby", born: "13/02/2021" },
+    { id: 3, name: "Amisha", born: "27/03/2003" },
+    { id: 4, name: "Grace", born: "13/02/2021" },
+    { id: 5, name: "Sally", born: "13/02/2021" },
+  ]);
+
+  const clear = () => {
+    setBirthdays([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="head">
+        <h1 className="head__title">Today's birthdays!</h1>
+      </div>
+      <div className="">
+        {birthdays.map((birthday) => {
+          if (birthday.born === new Date().toLocaleDateString()) {
+            return (
+              <p className="reminders__container" key={birthday.id}>
+                {birthday.name} 💜
+              </p>
+            );
+          } else {
+            return "";
+          }
+        })}
+      </div>
+      <button onClick={clear}>Clear All</button>
     </div>
   );
 }
